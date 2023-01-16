8040e715215794a4aa764d12e871ddc60450e1b2
Minin81 <nikolai.minin81@gmail.com>
2023-01-16 14:18:36 +0300

    Commit 01

---
+++ src/main/java/koschei/models/Deth8.java
@@ -0,0 +1,14 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Deth8 {
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+
+}
